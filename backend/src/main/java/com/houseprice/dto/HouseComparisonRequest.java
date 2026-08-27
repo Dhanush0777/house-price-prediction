5a4f86dd5eb2566056e82f23050790eac8e91313
Dhanush0777 <dhanushvulapelli@gmail.com>
2026-08-27 22:47:30 +0530

    Add backend/src/main/java/com/houseprice/dto/HouseComparisonRequest.java

---
+++ backend/src/main/java/com/houseprice/dto/HouseComparisonRequest.java
@@ -0,0 +1,21 @@
+package com.houseprice.dto;
+
+import java.util.List;
+
+public class HouseComparisonRequest {
+    private List<PredictionRequest> houses;
+
+    public HouseComparisonRequest() {}
+
+    public HouseComparisonRequest(List<PredictionRequest> houses) {
+        this.houses = houses;
+    }
+
+    public List<PredictionRequest> getHouses() {
+        return houses;
+    }
+
+    public void setHouses(List<PredictionRequest> houses) {
+        this.houses = houses;
+    }
+}
